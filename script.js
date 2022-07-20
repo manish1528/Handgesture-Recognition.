0e37e75e96041ad23b35782ba1e6f01a451d2c24
manish1528 <109609153+manish1528@users.noreply.github.com>
2022-07-20 17:21:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ function checkImageWithNyckel(image)
     formdata.append('file', image);
 
     $.ajax({
-        url:'https://www.nyckel.com/v1/functions/q2jjl438wnztxnel/invoke',
+        url:'https://www.nyckel.com/v1/functions/nimnxlxpo2p32dy6/invoke',
         type: 'post',
         data: formdata,
         contentType: false,
